d239aa70600097a63e0f4182c6bfa1427555adc1
RgregoR <gredelonghi@gmail.com>
2025-10-02 07:35:42 +0200

    update 20251002_0735_jbe

--- ff-launch/get-playlist-titles/get-playlist-titles.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-ydl='/usr/local/bin/yt-dlp --proxy http://10.91.8.21:80'
-
-while read LINE; do
-	echo -ne "$LINE\t\t -- "
-	$ydl -I 1:1 --skip-download --print playlist_title $LINE
-done < playlists-from-sites.txt
-
