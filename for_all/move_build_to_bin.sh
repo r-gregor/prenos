e23458613fee48908c14ca03c5aec0140cdc08fc
r-gregor <gredelonghi@gmail.com>
2025-11-14 10:29:25 +0100

    update 20251114_1029_en

--- for_all/move_build_to_bin.sh
+++
@@ -1,2 +0,0 @@
-for DDD in $(find * -type d -name build); do echo mv -v $DDD ${DDD//build/bin}; done
-
--- for_all/nw.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
- nohup /usr/bin/mintty -e /usr/bin/bash -c "~/majstaf_en/majbin_en/BrthReminder/BrthReminder_c_color.exe;cd;bash" 2> /dev/null
-
