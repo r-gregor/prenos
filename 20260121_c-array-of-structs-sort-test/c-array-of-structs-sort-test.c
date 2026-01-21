7b1fd8296b7a4f0fddea2fbe4db01857da1ff8cf
r-gregor <gredelonghi@gmail.com>
2026-01-21 16:09:07 +0100

    update 20260121_1609_en

---
+++ 20260121_c-array-of-structs-sort-test/c-array-of-structs-sort-test.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct object {
+	// char line[256];
+	char *line;
+	size_t size;
+	float flag;
+} Object_t ;
+
+void print_array(Object_t *arr, size_t size);
+void print_array_ptr(Object_t (*arr)[], size_t size);
+
+
+/* main */
+int main(int argc, char **argv) {
+
+	/* array of structs Object_t declaration + initialization */
+	Object_t array_of_structs[] = {
+		{"Name1", 5, 1.0},
+		{"AAjA", 12, 1.0},
+		{"GuGu", 4, 1.5},
+		{"nmcc1", 25, 0.5},
+		{"NASA", 51, 1.5},
+		{"IRS", 7, 1.5},
+		{"MaGa", 9, 1.0},
+		{"RgregoR", 1, 0.0},
+		{"NSI", 22, 1.5},
+		{"KISS", 105, 1.0}
+	};
+
+	size_t arr_size = sizeof(array_of_structs) / sizeof(array_of_structs[0]);
+
+	/* print-1: print an array of structs */
+	print_array(array_of_structs, arr_size);
+
+	/* --- */
+
+	/* pointer to an entire array of structs */
+	Object_t (*aos_ptr)[arr_size] = &array_of_structs;
+
+	/* print-2: print a pointer to an array of structs */
+	print_array_ptr(aos_ptr, arr_size);
+
+
+	return 0;
+} // end main
+
+/* printing array of structs */
+void print_array(Object_t *arr, size_t size) {
+	printf("Printing an array of structs Object_t:\n");
+	printf("%-10s ::  %4s  :: %5s\n",  "line", "size","flag");
+	printf("------------------------------\n");
+	for (int i=0; i < size; ++i) {
+		printf("%-10s ::  %4ld  :: %4.1f\n",  arr[i].line, arr[i].size, arr[i].flag);
+	}
+	printf("\n");
+}
+
+
+/* printing pointer to array of structs */
+void print_array_ptr(Object_t (*arr)[], size_t size) {
+	printf("Printing a pointer to an array of structs Object_t:\n");
+	printf("%-10s ::  %4s  :: %5s\n",  "line", "size","flag");
+	printf("------------------------------\n");
+	for (int i=0; i < size; ++i) {
+		printf("%-10s ::  %4ld  :: %4.1f\n",  (*arr + i)->line, (*arr + i)->size, (*arr + i)->flag);
+	}
+	printf("\n");
+}
+
