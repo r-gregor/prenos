e48516758f7b55280af5eb51e1cb47b26aae0de0
r-gregor <gredelonghi@gmail.com>
2026-01-25 13:14:18 +0100

    update 20260125_1314_mdb

--- update_mdb/find-all-shell-scripts.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-find -type f -exec file {} \; | grep "shell script" | awk -F : '{ print $1 }'
-# find -type f -iname "*\.sh" | xargs -I{} file {} | grep "shell script" | awk -F : '{ print $1 }'
-
