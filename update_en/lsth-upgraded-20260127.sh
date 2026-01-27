d1394854f8897e52a00733f96fa0ddfa5fbb1b83
r-gregor <gredelonghi@gmail.com>
2026-01-27 21:54:42 +0100

    update 20260127_2154_mdb

--- update_en/lsth-upgraded-20260127.sh
+++ update_en/lsth-upgraded-20260127.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-# fname: lsth-upgraded-20260126.sh
+# fname: lsth
 # 20260126 d: added optarg for path and number
 # last: 20260126
 # ---
@@ -7,10 +7,20 @@
 NUM=15
 DEST="."
 
-while getopts 'p:n:' opt; do
+while getopts 'p:n:h' opt; do
 	case "${opt}" in
 		p) DEST="${OPTARG}";;
 		n) NUM="${OPTARG}";;
+		h) cat <<"EOF"
+Usage: lsth -[p, n, h]
+	-p ... PATH
+	-n ... number of files
+	-h ... this help message
+
+	if no argument: current dir and max 15 files
+
+EOF
+	exit;;
 	esac
 done
 
