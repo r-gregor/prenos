941cd3d2481a614c0d8307ae66ef718cbd1a60b2
r-gregor <gredelonghi@gmail.com>
2026-03-19 15:47:53 +0100

    update 20260319_1547_en

---
+++ update_jbe/vview-vedit-commands/vview-mfjl-efzf.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+# fname: vview-mfjl-efzf.sh
+# 20260319  en v1
+# last: 20260319
+# ---
+
+vcmd='vim -Mp'
+fcmd='fzf -m --reverse -e'
+readarray -t selections < <(cd $HOME && ${fcmd})
+
+if [ "${#selections[@]}" -eq 0 ]; then
+	printf "[INFO] nothing selected\n\n"
+	exit
+fi
+
+printf "[INFO] selected:\n"
+for selection in "${selections[@]}"; do
+	printf "%s\n" "${HOME}/${selection}"
+done
+
+for selection in "${selections[@]}"; do
+	printf "${HOME}/${selection} "
+done | xargs -ro ${vcmd}
+
---
+++ update_mdb/vview-vedit-commands/vedit-mfjl-fzf.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+# fname: vedit-mfjl-fzf.sh
+# 20260319  en v1
+# last: 20260319
+# ---
+
+vcmd='vim -p'
+fcmd='fzf -m --reverse'
+readarray -t selections < <(cd $HOME && ${fcmd})
+
+if [ "${#selections[@]}" -eq 0 ]; then
+	printf "[INFO] nothing selected\n\n"
+	exit
+fi
+
+printf "[INFO] selected:\n"
+for selection in "${selections[@]}"; do
+	printf "%s\n" "${HOME}/${selection}"
+done
+
+for selection in "${selections[@]}"; do
+	printf "${HOME}/${selection} "
+done | xargs -ro ${vcmd}
+
