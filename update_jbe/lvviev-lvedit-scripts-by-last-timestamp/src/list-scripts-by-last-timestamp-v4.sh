c6dd88eab9fea2ca10be79f1897db3ef533cd86a
r-gregor <gredelonghi@gmail.com>
2026-05-21 11:05:39 +0200

    update 20260521_1105_en

---
+++ update_jbe/lvviev-lvedit-scripts-by-last-timestamp/src/list-scripts-by-last-timestamp-v4.sh
@@ -0,0 +1,96 @@
+#! /usr/bin/env bash
+# fname: list-scripts-by-last-timestamp.sh
+# v1_20260520
+# v2_20260520 add fzf single selection
+# v3_20260220 fzf to multiple selections to open in vim
+# v4_20260521 refactor parts of code into functions and introduce main()
+# last: 20260521
+# ---
+
+unset fjls_lst
+unset selections
+unset selection
+
+declare -a fjls_lst
+declare -a selections
+
+currdtstmp=$(date +"%Y%m%d")
+
+dest="/home/gregor.redelonghi/majstaf/majbin"
+FZFCMD_EN="fzf -e -m --reverse"   # cygwin version does not support --width option
+
+usage() {
+cat << "EOF"
+	Usage: list-scripts-by-last-timestamp <datestamp>
+		<datestamp> format: YYYYmmdd ("20260520")
+                            at least YYYYmm
+
+EOF
+}
+
+
+load_files_into_list() {
+	for FFF in $(find ${dest}/* -name "*\.sh" | grep -v 'src/'); do
+		dtstmp=$(grep last "$FFF" | grep -Eo "[0-9]{8}")
+		if [ $? -eq 0 ]; then
+			if [[ ${dtstmp} =~ ${djt} ]]; then
+				fjls_lst+=("${FFF};${dtstmp}")
+			else
+				continue
+			fi
+		else
+			continue
+		fi
+	done
+
+	if [ ${#fjls_lst[@]} -eq 0 ]; then
+		printf "[INFO] no file with datestamp: '%s' found\n\n" "${dtstmp}"
+		exit 1
+	fi
+
+	fjls_lst+=("---;${currdtstmp}")
+	fjls_lst+=("Quit;${currdtstmp}")
+}
+
+# v3
+main() {
+	if [ $# -ne 1 ]; then
+		usage
+		exit 1
+	elif [ ${#1} -gt 8 ] || [ ${#1} -lt 6 ]; then
+		usage
+		exit 1
+	else
+		djt="$1"
+		load_files_into_list
+	fi
+
+	selections+=$((for FJL in ${fjls_lst[@]}; do
+		while IFS=';' read fname dtstmp; do
+			echo "${fname}"
+		done < <(echo ${FJL})
+	done) | ${FZFCMD_EN})
+
+	if [ "${#selections[@]}" -eq 0 ]; then
+		printf "[INFO] nothing selected\n\n"
+		exit 1
+	fi
+
+	for selection1 in ${selections[@]}; do
+		if [[ "${selection1}" == "Quit" ]]; then
+			printf "[INFO] nothing selected\n\n"
+			exit 1
+		fi
+	done
+
+	printf "[INFO] Selected:\n"
+	for selection2 in ${selections[@]}; do
+		printf "${selection2}\n"
+	done
+	printf "\n"
+}
+
+
+#MAIN
+main "$@"
+
---
+++ update_mdb/lvviev-lvedit-scripts-by-last-timestamp/src/list-scripts-by-last-timestamp-v1.sh
@@ -0,0 +1,59 @@
+#! /usr/bin/env bash
+# fname: list-scripts-by-last-timestamp.sh
+# v1_20260520
+# v2_20260520 TODO: add fzf to vim stuff ...
+# last: 20260520
+# ---
+
+unset selections
+unset selection
+
+declare -a selections
+
+dest="/home/gregor.redelonghi/majstaf/majbin"
+
+usage() {
+cat << "EOF"
+	Usage: list-scripts-by-last-timestamp <datestamp>
+		<datestamp> format: YYYYmmdd ("20260520")
+                            at least YYYYmm
+
+EOF
+}
+
+if [ $# -ne 1 ]; then
+	usage
+	exit
+elif [ ${#1} -gt 8 ] || [ ${#1} -lt 6 ]; then
+	usage
+	exit
+else
+	djt="$1"
+fi
+
+for FFF in $(find ${dest}/* -name "*\.sh" | grep -v 'src/'); do
+	dtstmp=$(grep last "$FFF" | grep -Eo "[0-9]{8}")
+	if [ $? -eq 0 ]; then
+		if [[ ${dtstmp} =~ ${djt} ]]; then
+			selections+=("${FFF};${dtstmp}")
+		else
+			continue
+		fi
+	else
+		continue
+	fi
+done
+
+if [ ${#selections[@]} -eq 0 ]; then
+	printf "[INFO] no file with datestamp: '%s' found\n"
+	exit
+fi
+
+printf "[INFO] files found:\n"
+for FJL in ${selections[@]}; do
+	while IFS=';' read fname dtstmp; do
+		printf "${dtstmp} -- ${fname}\n"
+	done < <(echo ${FJL})
+done | sort -nr
+printf "\n"
+
