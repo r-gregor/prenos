b2c4e78fbac7a3bdf476052b3368beb89bcf11f3
r-gregor <gredelonghi@gmail.com>
2026-08-03 18:03:31 +0200

    update 20260803_1803_mdb

--- update_jbe/list-new-files-from-n-days-from-today/list-new-files-from-n-days-from-today-v2-mdb.sh
+++ update_jbe/list-new-files-from-n-days-from-today/list-new-files-from-n-days-from-today-v2-mdb.sh
@@ -21,14 +21,23 @@ else
 	ddiff=${1}
 fi
 
-newd=$((DY - ddiff))
+new_DY=$(( DY - ddiff ))
+new_MN=${MN//0/}
 
-if [ $newd -le 0 ]; then
+# test
+# echo "new_DY: ${new_DY}"
+# read -p "OK?"
+
+if [ $new_DY -le 0 ]; then
 	printf "[ERROR] The days difference is to big\n"
 	exit 1
 fi
 
-newdate=${YR}${MN}${newd}
+newdate=$(printf "%4d%02d%02d\n" "${YR}" "${new_MN}" "${new_DY}")
+
+# test
+# echo "newdate: ${newdate}"
+# read -p "OK?"
 
 # -newermt: m - modification time
 # -newerBt: B - birth time
