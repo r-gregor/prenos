96415114f00ead485b7b3c1f7b6a5304512fc6fc
RgregoR <gredelonghi@gmail.com>
2026-08-06 23:27:09 +0200

    update 20260806_2327_jbe

--- update_jbe/polybar-restart.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-if [ "$(pgrep polybar)" == "" ]; then
-	printf "[INFO] polybar not running -- reloading ...\n"
-	/home/rgregor/.config/polybar/launch.sh
-else
-	printf "[INFO] polybar already running\n"
-fi
-
