cd4237b3ee47d454b72ac6aafd51102caf84c349
r-gregor <gredelonghi@gmail.com>
2026-08-26 15:38:15 +0200

    update 20260826_1538_en

---
+++ update_jbe/c-strings-are-terrible-20260826/just-build.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+# fname: build-and-run.sh
+# 20260826 v1
+# ---
+
+if [ ! -d './bin/' ]; then
+	mkdir -v ./bin
+fi
+
+gcc -o ./bin/c-string-to-string-view-testing c-string-to-string-view-testing.c
+
+
