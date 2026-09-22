7ef416f2b32b44f5c34daff116620877159bc6ff
r-gregor <gredelonghi@gmail.com>
2026-09-22 07:45:07 +0200

    update 20260922_0745_mdb

---
+++ update_en/majcommands-list-20260922/majcommands-list.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+# fname: majcommands-list.sh
+# descpt: make list of majcommands with decription
+# 20260921 v1
+# last: 20260921
+# ---
+
+unset found_bash_commands
+declare -a found_bash_commands
+
+# PTH='/home/gregor.redelonghi/majstaf/majbin/'
+PTH='/home/rgregor/majstaf/majbin/'
+SRC="${PTH}/git-commands"
+
+# PTH_ESCPD='\/home\/gregor.redelonghi\/majstaf\/majbin\/'
+# PTH_ESCPD="${PTH//\//\\/}"
+
+for FFF in $(find "${PTH}"/git-commands/*  "${PTH}"/ff*/* -maxdepth 0 -type f -name "*\.sh"); do
+	# INSERT="${FFF//${PTH_ESCPD}/}"
+	INSERT="${FFF//${PTH}/}"
+	found_bash_commands+=( "${INSERT/\//}" )
+done
+
+# echo "${found_bash_commands[@]}"
+# read -p "OK?"
+
+for CMD in "${found_bash_commands[@]}"; do
+	printf "%-60s %s\n" "${CMD}" "$(grep 'descpt' "${PTH}/${CMD}" | sed 's/# descpt: //')"
+done
+
